b0ae6615eb649b80ce01c5e44a00ea8b414f585d
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-26 00:52:46 +0200

    fix: test

--- src/utils/2a/index.spec.ts
+++ src/utils/2a/index.spec.ts
@@ -1,7 +1,7 @@
 import { getTeamArticlesDescription } from '.';
 
 const string_1 = 'Oliver wrote an article RxJS and redux-observable.\nJan wrote articles RxJS and redux-observable, Firebase and 1 more.\nJakub wrote articles MobX in practise, RxJS and redux-observable and 2 more.\nPeter wrote 0 articles.';
-const string_2 = 'Tomas wrote an article RxJS and redux-observable, CSS in JS.\nDrahoslav wrote an article RxJS and redux-observable.\nHonza wrote an article RxJS and redux-observable, Firebase.\n';
+const string_2 = 'Tomas wrote articles RxJS and redux-observable and CSS in JS.\nDrahoslav wrote an article RxJS and redux-observable.\nHonza wrote articles RxJS and redux-observable and Firebase.';
 
 describe('2a getTeamArticlesDescription', () => { 
   it('getTeamArticlesDescription(1)', () => {
